3a5a75a002dae67744cd49d904dcdd1882ea595c
Ugo <ugocruise@gmail.com>
2023-10-02 17:32:34 +0100

    add flip card

--- src/components/MainQuiz.jsx
+++ src/components/MainQuiz.jsx
@@ -8,72 +8,42 @@ function MainQuiz() {
   const { score, setScore, setGameState } = useContext(QuizContext);
   const [currentQuestion, setCurrentQuestion] = useState(0);
 
-  const handleClick = (pillType) => () => setTimeout (()=>{
-    //check selected pill
-    if (currentQuestion === Questions.length - 1) {
-      // setScore(score)
-      setGameState("end");
-      // return;
-    }
+  const handleClick = (pillType) => () =>
+    setTimeout(() => {
+      // let randomizer = Math.floor(Math.random()*Questions.length);
 
-    // console.log(score);
-    if (pillType === "red") {
-      setScore(score - 1);
-      // setCurrentQuestion(currentQuestion + 1);
-    }
+      //check selected pill
+      if (currentQuestion === Questions.length - 1) {
+        setGameState("end");
+      }
 
-    if (pillType === "blue") {
-      setScore(score + 1);
-      // console.log(score);
-    }
-    setCurrentQuestion(currentQuestion + 1);
-    
-  }, 800);
+      if (pillType === "red") {
+        setScore(score - 1);
+      }
 
-  // const nextQuestion = (pillType) => {
-  //   if (pillType === "blue") {
-  //     setScore(score + 10);
-  //     console.log("babalue");
-  //   }
-
-  //  // if (pillType === "red") {
-  //   //   setScore(score + 10);
-  //   //   console.log("babalue");
-  //   //
-  //   // }
-  //   // if (
-  //   //   pillType === "blue" &&
-  //   //   currentQuestion === Questions[currentQuestion].length - 1
-  //   // ) {
-  //   // }
-  //   // setCurrentQuestion(currentQuestion + 1);
-  //   // setGameState("end");
-  // };
+      if (pillType === "blue") {
+        setScore(score + 1);
+      }
+      setCurrentQuestion(currentQuestion + 1);
+    }, 800);
 
   return (
-
-      <motion.div className="main" initial={{opacity: 0}}
-      animate={{opacity: 1}} exit={{opacity: 0}}
-      >
+    <motion.div
+      className="main"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <h1 className="main-question">{Questions[currentQuestion].dilemma}</h1>
       <section className="question-card">
-        
         {" "}
         <div className="left button" onClick={handleClick("red")}>
           {Questions[currentQuestion].good}
         </div>
-                <div className="right button" onClick={handleClick("blue")}>
+        <div className="right button" onClick={handleClick("blue")}>
           {Questions[currentQuestion].evil}
         </div>
       </section>
-      {/* <div className="answers">
-        
-        <div>
-          <button className="left" onClick={handleClick("red")}></button>
-
-          <button className="right" onClick={handleClick("blue")}></button>
-        </div>
-      </div> */}
     </motion.div>
   );
 }
--- src/components/QuizEnd.jsx
+++ src/components/QuizEnd.jsx
@@ -1,42 +1,118 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import { QuizContext } from "../extras/Contexts";
+import ReactCardFlip from "react-card-flip";
+import Spinner from "../extras/Spinner";
 
 function QuizEnd() {
   const { score, setScore, setGameState } = useContext(QuizContext);
+  const [flip, setFlip] = useState(false);
+  const [loading, setLoading] = useState(true);
+  // const [message, setMessage] = useState("");
+
+  //FIXME: not working
+
+  const renderColor = () => {
+    let textColor = "red",
+      failColor = "purple";
+    // message = document.getElementById("side-two").textContent = "hello";
+
+    if (score <= 4) {
+      return textColor;
+    }
+
+    if (score >= 7) {
+      return failColor;
+    }
+  };
+
+  // const renderResponse = () => {
+  //   let message = (document.getElementById("side-one").innerHTML =
+  //     "<div>You failed</div>");
+  //   if (score <= 4) {
+  //     return message;
+  //   }
+
+  //   if (score >= 7) {
+  //     return "You are going to hell";
+  //   }
+
+  //   setFlip(flip);
+  // };
+  //TODO:
+
+  // useEffect(() => {
+  //   const element = document.getElementById("untow");
+  //   element.style.backgroundColor = "white";
+  //   element.style.padding = "100px";
+  //   element.style.borderRadius = "20px";
+  //   element.style.margin = "20px";
+
+  //   setScore(score);
+  // }, []);
 
   const restartQuiz = () => {
     setScore(0);
     setGameState("menu");
+    setLoading();
   };
 
-  const renderColor = () => {
-    if (score <= 4) {
-      return "red";
-    } else if (score <= 7) {
-      return "purple";
-    } else {
-      return "blue";
-    }
-  };
+  if (loading) {
+    return (
+      <div className="end">
+        <h1>End of Quiz</h1>
+        <ReactCardFlip isFlipped={flip} flipDirection="horizontal">
+          <div id="side-one" style={{ color: renderColor() }}>
+            <button
+              style={{
+                width: "250px",
+                margin: "20px",
+                height: "250px",
+                padding: "10px",
+                fontSize: "20px",
+                background: "#f5d9fa",
+                fontWeight: "bold",
+                borderRadius: "20px",
+              }}
+              onClick={() => setFlip(!flip)}
+            >
+              Your score is: {score}{" "}
+            </button>
+          </div>
+          <div id="side-two">
+            <button
+              style={{
+                width: "250px",
+                height: "250px",
+                margin: "20px",
+                padding: "10px",
+                fontSize: "20px",
+                background: "#f5d9fa",
+                fontWeight: "bold",
+                borderRadius: "20px",
+                color: renderColor(),
+              }}
+              onClick={() => {
+                setFlip(!flip);
+              }}
+            >
+              You are
+            </button>
+          </div>
+        </ReactCardFlip>
 
-  useEffect(() => {
-    const element = document.getElementById("tow");
-    element.style.backgroundColor = "red";
-    element.style.padding="50px";
-    element.style.borderRadius="20px";
-    element.style.margin ="20px"
-    
-  }, []);
-
-  return (
-    <div className="end">
-      <h1>End of Quiz</h1>
-      <div id="tow" onClick={renderColor}>{score}</div>
-      <button className="start-button" onClick={restartQuiz}>
-        Start Again
-      </button>
-    </div>
-  );
+        <button className="start-button" onClick={restartQuiz}>
+          Start Again
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <img src="spinner.gif" alt="spinner" />
+        <Spinner size={30}></Spinner>
+      </div>
+    );
+  }
 }
 
 export default QuizEnd;
